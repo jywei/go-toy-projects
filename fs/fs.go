20660a9eea353f9ae793a74d0166253b2b3d8e88
Roy Wei <roywjy@gmail.com>
2019-04-04 23:18:32 +1100

    Update to a basic file server

--- fs/fs.go
+++ fs/fs.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 	"os"
 )
 
 func main() {
-	dir, _ := os.Getwd()
-	http.ListenAndServe(":3000", http.FileServer(http.Dir(dir)))
+	var dir string
+	port := flag.String("port", "3000", "port to serve HTTP on")
+	// it's actually a pointer to the string
+	path := flag.String("path", "", "path to serve")
+	flag.Parse()
+
+	// Need to dereference the pointer
+	if *path == "" {
+		dir, _ = os.Getwd()
+	} else {
+		dir = *path
+	}
+
+	http.ListenAndServe(":"+*port, http.FileServer(http.Dir(dir)))
 }
+
+// go run fs.go -port=5151 -path=site
