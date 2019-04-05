c2c90af4fc9050ea1f530c2e86cf89b5a05317e1
Roy Wei <roywjy@gmail.com>
2019-04-05 23:12:40 +1100

    Remove unrelevant project

--- cms/template.go
+++ cms/template.go
@@ -5,18 +5,32 @@ import (
 	"time"
 )
 
-// Tmpl is an exported variable
-var Tmpl = template.Must(template.ParseGlob("../templates/*"))
+// Tmpl is a reference to all of our templates
+// ParseGlob would return a template and error, and Must will do the eror checking
+var Tmpl = template.Must(template.ParseGlob("templates/*"))
 
+// Page is the struct used for each webpage
 type Page struct {
+	ID      int
 	Title   string
 	Content string
 	Posts   []*Post
 }
 
+// Post is the struct used for each blog post
 type Post struct {
+	ID            int
 	Title         string
 	Content       string
 	DatePublished time.Time
 	Comments      []*Comment
+}
+
+// Comment is the struct used for each comment
+type Comment struct {
+	ID            int
+	PostID        int
+	Author        string
+	Comment       string
+	DatePublished time.Time
 }
