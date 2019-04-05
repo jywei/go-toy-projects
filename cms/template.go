71b5f41ada6b5b2f8eb1854f22aa14370e5e9d86
Roy Wei <roywjy@gmail.com>
2019-04-05 21:37:24 +1100

    Create a Post type

--- cms/template.go
+++ cms/template.go
@@ -1,6 +1,9 @@
 package cms
 
-import "html/template"
+import (
+	"html/template"
+	"time"
+)
 
 // Tmpl is an exported variable
 var Tmpl = template.Must(template.ParseGlob("../templates/*"))
@@ -8,4 +11,12 @@ var Tmpl = template.Must(template.ParseGlob("../templates/*"))
 type Page struct {
 	Title   string
 	Content string
+	Posts   []*Post
+}
+
+type Post struct {
+	Title         string
+	Content       string
+	DatePublished time.Time
+	Comments      []*Comment
 }
