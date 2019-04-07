170e2ebfaf1a43af0656e803b974a75095ff5487
Roy Wei <roywjy@gmail.com>
2019-04-07 21:06:35 +1000

    Add logger for logging

--- middleware/analytics.go
+++ middleware/analytics.go
@@ -1,40 +1,63 @@
 package middleware
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
-// Add is a variadic function that adds up numbers
-func Add(nums ...int) int {
-	sum := 0
-	for _, num := range nums {
-		sum += num
+// CreateLogger creates a new logger that writes to the given filename
+func CreateLogger(filename string) *log.Logger {
+	// os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666 will create a new file if not existing and grant the permission
+	file, err := os.OpenFile(filename+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
 	}
-	return sum
+	logger := log.New(file, "", log.Ldate|log.Ltime|log.Lshortfile)
+	return logger
 }
 
-// Chain holds the sum
-type Chain struct {
-	Sum int
-}
-
-// AddNext is chainable sum function
-func (c *Chain) AddNext(num int) *Chain {
-	c.Sum += num
-	return c
-}
-
-// Finally is to return the final sum of the chain
-func (c *Chain) Finally(num int) int {
-	return c.Sum + num
-}
-
-// Next runs the next function in the chainable
-func Next(next http.HandlerFunc) http.Handler {
+// Time runs the next function in the chain
+func Time(logger *log.Logger, next http.HandlerFunc) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Before")
+		start := time.Now()
 		next.ServeHTTP(w, r)
-		fmt.Println("After")
+		elapsed := time.Since(start)
+		logger.Println(elapsed)
 	})
 }
+
+// // Add is a variadic function that adds up numbers
+// func Add(nums ...int) int {
+// 	sum := 0
+// 	for _, num := range nums {
+// 		sum += num
+// 	}
+// 	return sum
+// }
+//
+// // Chain holds the sum
+// type Chain struct {
+// 	Sum int
+// }
+//
+// // AddNext is chainable sum function
+// func (c *Chain) AddNext(num int) *Chain {
+// 	c.Sum += num
+// 	return c
+// }
+//
+// // Finally is to return the final sum of the chain
+// func (c *Chain) Finally(num int) int {
+// 	return c.Sum + num
+// }
+//
+// // Next runs the next function in the chainable
+// func Next(next http.HandlerFunc) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Println("Before")
+// 		next.ServeHTTP(w, r)
+// 		fmt.Println("After")
+// 	})
+// }
--- middleware/cmd/main.go
+++ middleware/cmd/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/jywei/toy-projects/middleware"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Executing...")
+	// fmt.Println("Executing...")
 	w.Write([]byte("Hello"))
 }
 
 func main() {
-	sum := middleware.Add(1, 2, 3)
-	fmt.Println(sum)
+	// sum := middleware.Add(1, 2, 3)
+	// fmt.Println(sum)
+	//
+	// chain := &middleware.Chain{0}
+	// sum2 := chain.AddNext(1).AddNext(2).AddNext(3).Finally(0)
+	// fmt.Println(sum2)
 
-	chain := &middleware.Chain{0}
-	sum2 := chain.AddNext(1).AddNext(2).AddNext(3).Finally(0)
-	fmt.Println(sum2)
-
-	http.Handle("/", middleware.Next(hello))
+	// http.Handle("/", middleware.Next(hello))
+	logger := middleware.CreateLogger("section4")
+	http.Handle("/", middleware.Time(logger, hello))
 	http.ListenAndServe(":3000", nil)
 }
