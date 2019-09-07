60ecb01e5e9a3b829ca8ce56320de4ff8775d217
Roy Wei <roywjy@gmail.com>
2019-09-07 22:24:33 +1000

    Implement toString() function

--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 
 	// cards.print()
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+
+	fmt.Println(cards.toString())
 }
 
 func newCard() string {
