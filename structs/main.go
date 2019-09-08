264b20c01140252b3c876ef525f123dd9f925b2f
Roy Wei <roywjy@gmail.com>
2019-09-08 14:43:24 +1000

    Add a struct inside a struct

--- structs/main.go
+++ structs/main.go
@@ -2,18 +2,35 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	// alex := person{firstName: "Alex", lastName: "Anderson"}
-	var alex person
+	// var alex person
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Harper",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 }
