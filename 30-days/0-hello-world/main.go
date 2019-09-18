c6b6590187ef43743b1e4fc90ac982d02be7aa62
Roy Wei <roywjy@gmail.com>
2019-09-19 00:06:45 +1000

    Add 0-hello-world

---
+++ 30-days/0-hello-world/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	inputreader := bufio.NewReader(os.Stdin)
+	input, _ := inputreader.ReadString('\n')
+	fmt.Println("Hello, World.")
+	fmt.Println(input)
+}
