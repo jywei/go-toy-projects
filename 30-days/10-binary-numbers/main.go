230f0cb1e2a256e6e9f935b9c4124122aacbf12a
Roy Wei <roywjy@gmail.com>
2019-09-29 17:35:23 +1000

    Add split binary

---
+++ 30-days/10-binary-numbers/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+	b := strconv.FormatInt(int64(n), 2)
+	arrayString := strings.Split(b, "0")
+	var max int
+	for _, value := range arrayString {
+		if l := len(value); l > max {
+			max = l
+		}
+	}
+	fmt.Println(max)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
